baf3b0845c4711616ee1d56f1dd13808fe98de5e
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-05-19 19:17:58 -0700

    oh doh

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,30 @@ import slice from 'lodash/slice'
 import Chart from '../components/chart'
 
 const cx = {
-  main: 'vh-100 dt w-100 bg-animate',
+  main: 'vh-100 dt w-100',
   container: 'dtc v-mid tc',
   article: 'center',
-  label: 'f2 sans-serif fw2 ph2'
+  label: 'f2 sans-serif fw2 ph2 animate'
 }
 
 const Index = () => {
   const [hz, setHz] = useState(0)
   const [hzHistory, setHzHistory] = useState([])
-  const handleHz = useCallback(throttle(setHz, 60), [hz])
-  pitch(handleHz)
+  const handleHz = useCallback(throttle(setHz, 200), [hz])
   useEffect(() => {
-    setHzHistory(slice(hzHistory.concat([{ hz, date: Date.now() }]), -100))
+    pitch(handleHz)
+  }, [])
+  useEffect(() => {
+    setHzHistory(slice(hzHistory.concat([{ hz, date: Date.now() }]), -80))
   }, [hz])
 
   return (
-    <Layout className={`${cx.main} ${hz <= 150 && hz !== 0 ? 'bg-dark-red' : ''}`}>
+    <Layout className={cx.main}>
       <div className={cx.container}>
         <article className={cx.article}>
           <div id='pitch'>
             <Chart data={hzHistory} />
-            <span className={cx.label}>{hz}{'hz'}</span>
+            <span className={`${cx.label} ${hz <= 150 && hz !== 0 ? 'dark-red' : ''}`}>{hz}{'hz'}</span>
           </div>
         </article>
       </div>
--- utils/pitch.js
+++ utils/pitch.js
@@ -1,30 +1,23 @@
 import Pitchfinder from 'pitchfinder'
-import has from 'lodash/has'
 import round from 'lodash/round'
 
+const detectPitch = new Pitchfinder.AMDF()
+const isBrowser = typeof window !== 'undefined'
+
 const pitch = (cb) => {
-  if (typeof window !== 'undefined') {
-    const detectPitch = new Pitchfinder.AMDF()
-    const hasStandardAudioContext = has(window, 'AudioContext')
-    const hasWebkitAudioContext = has(window, 'webkitAudioContext')
-    const isWebkitAudio = !hasStandardAudioContext && hasWebkitAudioContext
-
-    const update = (e) => {
-      const float32Array = e.inputBuffer.getChannelData(0)
-      const p = detectPitch(float32Array)
-      if (p > 40 && p < 600) {
-        cb(round(p))
-      }
-    }
-
+  if (isBrowser) {
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-      const AudioContext = !isWebkitAudio ? window.AudioContext : window.webkitAudioContext
-      const audioCtx = new AudioContext()
+      const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
       const source = audioCtx.createMediaStreamSource(stream)
-      const processor = audioCtx.createScriptProcessor(256, 1, 1)
+      const processor = audioCtx.createScriptProcessor(2048, 1, 1)
       source.connect(processor)
       processor.connect(audioCtx.destination)
-      processor.onaudioprocess = update
+      processor.onaudioprocess = (e) => {
+        const p = round(detectPitch(e.inputBuffer.getChannelData(0)))
+        if (p && p >= 0 && p <= 600) {
+          cb(p)
+        }
+      }
     })
   }
 }
