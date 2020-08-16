34fa874bd2aa8c341702b7029c2fedbf248bc59a
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-08-15 19:58:29 -0700

    css purge

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
+const withCss = require('@zeit/next-css')
+const withPurgeCss = require('next-purgecss')
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching
-  }
-})
+module.exports = withPlugins(
+  [
+    [withPWA, {
+      pwa: {
+        dest: 'public',
+        runtimeCaching
+      }
+    }],
+    [withCss],
+    [withPurgeCss]
+  ]
+)
