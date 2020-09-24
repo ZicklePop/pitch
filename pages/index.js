9c89103d7dd4390dc01c1d89b56cf1db76444178
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-09-24 10:24:34 -0700

    nits

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react'
 import Chart from '../components/chart'
 import Layout from '../components/layout'
 import pitch from '../utils/pitch'
